e094ea87bff8600d4a961206c5e8d3dcc7f23a51
Rankush <iamrankush@gmail.com>
2024-08-11 22:19:12 +0530

    currying program

---
+++ currying.js
@@ -0,0 +1,15 @@
+function f(x) {
+  x = "x-" + x;
+  return function (y) {
+    y = "y-" + x;
+    return function (z) {
+      return (z = "z-" + y);
+    };
+  };
+}
+
+let g = f("a")("b")("c");
+
+console.log(g);
+
+//output: z-y-x-a
