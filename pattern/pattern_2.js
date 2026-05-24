b3a7921e7168f04eddcc369a7cd3554ac3e83c14
Rankush <iamrankush@gmail.com>
2026-05-24 23:00:14 +0530

    programs added

--- pattern/pattern_2.js
+++ pattern/pattern_2.js
@@ -6,6 +6,13 @@
 
 // input : 4
 
+// 2nd question
+// Output :
+// * * * *
+// * * *
+// * *
+// *
+
 const starPattern = (n) => {
   for (let i = 0; i < n; i++) {
     let row = "";
@@ -18,3 +25,20 @@ const starPattern = (n) => {
 };
 
 starPattern(4);
+
+console.log("_________________________");
+console.log("2nd Ouptut");
+
+const starPattern2 = (num) => {
+  for (let i = 0; i < num; i++) {
+    let row = " ";
+
+    for (let j = 0; j < num - i; j++) {
+      row += "*" + " ";
+    }
+
+    console.log(row);
+  }
+};
+
+starPattern2(5);
--- pattern/pattern_3.js
+++ pattern/pattern_3.js
@@ -8,6 +8,16 @@
 // Approach 1 : Using nested loop
 // Input : 5
 
+// Output 2 :
+// 1 2 3 4 5
+// 1 2 3 4
+// 1 2 3
+// 1 2
+// 1
+
+// Approach 2 : Using nested loop
+// Input : 5
+
 function pattern(num) {
   for (let i = 0; i < num; i++) {
     let row = " ";
@@ -20,3 +30,18 @@ function pattern(num) {
 }
 
 pattern(5);
+
+console.log("_______________________________");
+console.log("Output 2 :");
+
+function pattern2(num) {
+  for (let i = 0; i < num; i++) {
+    let row = " ";
+    for (let j = 0; j < num - i; j++) {
+      row += j + 1 + " ";
+    }
+    console.log(row);
+  }
+}
+
+pattern2(5);
